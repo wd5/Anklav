a6c3f6a79a216eec1dffc2e395eb76fa8b891307
Mikhail Polykovskij <glader@yandex-team.ru>
2012-09-04 20:09:23 +0600

    Email black list

--- src/core/utils.py
+++ src/core/utils.py
@@ -2,9 +2,12 @@
 
 from django.core.mail import send_mail
 
+black_list = ['silveri@mail.ru']  # Явно попросившие ничего им не присылать
+
 def email(subject, content, recipients):
     for mail in recipients:
-        send_mail(subject, content, None, [mail])
+        if mail not in black_list:
+            send_mail(subject, content, None, [mail])
 
     for mail in ('linashyti@gmail.com', 'glader.ru@gmail.com'):
         if not mail in recipients:

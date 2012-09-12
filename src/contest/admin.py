10476121fe5f7b0dec30f789dc6fc5c74241a14d
Mikhail Polykovskij <glader@yandex-team.ru>
2012-09-13 00:06:11 +0600

    Contest admin

--- src/contest/admin.py
+++ src/contest/admin.py
@@ -2,4 +2,9 @@
 from django.contrib import admin
 from .models import *
 
+class ContestVoteAdmin(admin.ModelAdmin):
+    list_display = ('author', 'member')
+    list_filter = ('member',)
+
 admin.site.register(ContestMember)
+admin.site.register(ContestVote, ContestVoteAdmin)

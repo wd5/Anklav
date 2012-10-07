01d38f712d1fb2df76d1ad8ecc9115a17fd9aa31
Mikhail Polykovskij <glader@yandex-team.ru>
2012-10-07 12:43:04 +0600

    Urls fix

--- src/news/urls.py
+++ src/news/urls.py
@@ -7,9 +7,9 @@ from django.views.generic.list import ListView
 from .models import News, CommonNews
 
 urlpatterns = patterns('',
-    url(r'^/site$', ListView.as_view(queryset=News.objects.all()), name='site_news'),
-    url(r'^/common$', 'news.views.common_news', name='common_news'),
-    url(r'^/common/(?P<pk>\d+)$', DetailView.as_view(model=CommonNews), name='common_news_item'),
+    url(r'^/site.html$', ListView.as_view(queryset=News.objects.all()), name='site_news'),
+    url(r'^/common.html$', 'news.views.common_news', name='common_news'),
+    url(r'^/common/(?P<pk>\d+).html$', DetailView.as_view(model=CommonNews), name='common_news_item'),
     url(r'^/common/(?P<pk>\d+)/edit$', 'news.views.edit_common_news', name='common_news_item_edit'),
-    url(r'^/common/add$', 'news.views.add_common_news', name='common_news_add'),
+    url(r'^/common/add.html$', 'news.views.add_common_news', name='common_news_add'),
 )
